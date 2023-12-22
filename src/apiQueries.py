12193f776a0338146f0ec927d4e5138e5f465dcc
david.fontesjunqueirasdffontesgit config --global user.email david.fontesjunqueira@epfl.ch <david.fontesjunqueira@epfl.ch>
2023-12-22 20:23:44 +0100

    up to date querries + details

--- src/apiQueries.py
+++ src/apiQueries.py
@@ -4,13 +4,21 @@ import time
 from SPARQLWrapper import SPARQLWrapper, JSON
 import tmdbsimple as tmdb
 import multiprocessing
+import sys
 
 wiki_Qrl = 'https://query.wikidata.org/sparql'
+
+# Ref : https://stackoverflow.com/questions/5980042/how-to-implement-the-verbose-or-v-option-into-a-script
+def _verboseprint(*args):
+    # Print each argument separately so caller doesn't need to
+    # stuff everything to be printed into a single string
+    for arg in args:
+        print(arg)
 
 def compare_titles(title1,title_list):
     '''
     Checks if there is a match between a str and a list of str objects.
-    The comparison is performed in lower cases
+    The process is performed in lower cases
     '''
     list_lower = [x.lower() for x in title_list]
     if title1.lower() in list_lower:
@@ -18,71 +26,111 @@ def compare_titles(title1,title_list):
     else:
         return False
     
-def match_movies(tmdb_response, querried_movie):
-    '''
-    Checks more precisely in a tmdb request if the requested movie has an 'exact' match
-    A match occurs when titles (in lowercases) and runtime (+- 3min) are similar 
+def set_TMDB_key(key):
+    tmdb.API_KEY = key
 
-    '''
-    for movies in tmdb_response['results']:
-        q_title, q_runtime = querried_movie
-        movie_details = tmdb.Movies(movies['id']).info()
-        movie_title = movie_details['title'] 
-        movie_original_title = movie_details['original_title']
-        movie_runtime = movie_details['runtime']
-        if ((compare_titles(q_title,[movie_title,movie_original_title])) 
-            and (abs(q_runtime - movie_runtime)<=3)):
-            return [q_title,
-                    movie_details['vote_average'],
-                    movie_details['vote_count'],
-                    movie_details['budget']]
-    return None
+def multiprocess_query(movie_list, task, nb_workers = 5):
+    """Allows to run the queries to TMDB in parallel
 
-def multiprocess_query(movie_list, api_key, nb_workers = 5, task = None):
-    '''
-    Multitasks the query to the TMDB database.
-    '''
-    # Split dataset in chunk of equal size for workers
-    chunk_size = len(movie_list) // nb_workers 
-    chunks = [movie_list[i:i+chunk_size] for i in range(0, len(movie_list), chunk_size)] 
-    # Create a pool with the specified number of workers and the task to accomplish
-    pool = multiprocessing.Pool(nb_workers) 
-    results = pool.map(task,(chunks,api_key))
-    # Wait for all workers to end and concatenate their results
-    pool.join() 
-    merged_df = pd.concat(results) 
+    Args:
+        movie_list (pd.Dataframe): Dataframe with the required info to run the queries.
+        task (fun): fun that will be applied to each dataframe element.
+        nb_workers (int, optional): Number of subproccesses running. Defaults to 5.
+
+    Returns:
+        pd.Dataframe: Dataframe containing the TMDB info of every querried movie.
+    """    
+    num_workers = nb_workers # number of workers you want to use
+    chunk_size = len(movie_list) // num_workers # size of each chunk
+    chunks = [movie_list[i:i+chunk_size] for i in range(0, len(movie_list), chunk_size)] # split the dataframe into chunks
+    pool = multiprocessing.Pool(num_workers) # create a pool of workers
+    results = pool.map(task,chunks) # apply the worker function to each chunk
+    pool.close() # close the pool
+    pool.join() # wait for all the workers to finish
+    merged_df = pd.concat(results) # concatenate the dataframes returned by each worker
     return merged_df
 
-def getInfoFromTMDB(movie_list,key = None,verbose = False):
-    '''
-    Takes a dataframe of movie titles along with durations and returns the 
-    vote_counts, vote_averages and budget all the matches found in the 
-    TMDB database
-    '''
-    #Define the verbose function to print informations during the process
+def match_movies(tmdb_response, querried_movie):
+    """Checks in a tmdb request if one of the movies is an 'exact' match
+    A match occurs when titles (in lowercase letters) and runtime (+- 3min) 
+    are similar 
+
+    Args:
+        tmdb_response (dict): response from the TMDB server
+        querried_movie (_type_): Information about the movie that needs to be matched to a tmdb entry
+
+    Returns:
+        array: information extracted from tmdb
+    """
+    for movies in tmdb_response['results']:
+        q_title, q_runtime, q_id = querried_movie
+        try:
+            time.sleep(0.01)
+            movie_details = tmdb.Movies(movies['id']).info()
+            movie_title = movie_details['title'] 
+            movie_original_title = movie_details['original_title']
+            movie_runtime = movie_details['runtime']
+            if ((compare_titles(q_title,[movie_title,movie_original_title])) 
+                and (abs(q_runtime - movie_runtime)<=3)):
+                return [q_title,
+                        q_id,
+                        movie_details['vote_average'],
+                        movie_details['vote_count'],
+                        movie_details['budget']]
+        except requests.exceptions.HTTPError as e:
+            print(e, file=sys.stdout)
+        except Exception:
+            print('General exception caught')
+            print(e, file=sys.stdout)
+    return None
+
+
+def getInfoFromTMDB(movie_list,verbose = False):
+    """Fetches a movie list from tmdb and return useful info
+
+    Args:
+        movie_list (pd.Dataframe): Movie dataframe with the info needed to perform matchings
+        verbose (bool, optional):  prints additional information. Defaults to False.
+
+    Returns:
+        pd.Dataframe: dataframe with the info from tmdb for all querried movies
+    """    
     if verbose:
         vprint = _verboseprint
     else:
         vprint = lambda *a: None
     vprint('Starting to fetch ...')
-    tmdb.API_KEY = key
     matched_results = []
     for idx, query in movie_list.iterrows():
-        # Search for the list of movies containing the queried name
-        search = tmdb.Search()
-        response = search.movie(query=query[0])
-        match = match_movies(response,query)
-        if match:
-            matched_results.append(match)
-            vprint('Found a match for movie number {}\n{}'.format(idx,match))
-    info_df = pd.DataFrame(data = matched_results,columns=['title','vote_average',
-                                                           'vote_counts','budget'])
+        if idx%1000 == 0:
+            print('Fetching movie number {} out of {}'.format(idx,len(movie_list)))
+        time.sleep(0.01)
+        try:
+            search = tmdb.Search()
+            response = search.movie(query=query[0])
+            match = match_movies(response,query)
+            if match:
+                matched_results.append(match)
+                vprint('Found a match for movie number {}\n{}'.format(idx,match))
+        except Exception:
+            print('Caught an exception on search for the movie:\n{}'.format(query))
+    info_df = pd.DataFrame(data = matched_results,columns=['title','movie_freebase_id','rating_average',
+                                                           'rating_count','budget'])
     return info_df
+                
+
 
 def getLabelFromFBID(id_list, verbose = False):
-    '''Takes a list of Freebase IDs as input and returns
-    a Pandas dataframe with the according freebase IDs labels
-    '''
+    """Takes a list of Freebase IDs as input and returns
+    a Pandas dataframe with the according freebase IDs
+
+    Args:
+        id_list (list): list containing freebase ids
+        verbose (bool, optional): prints additional information. Defaults to False.
+
+    Returns:
+        pd.Dataframe: dataframe containing freebase ids with their corresponding labels.
+    """    
     # Allows to print items only if verbose is set to True
     # Does nothing in case of false 
     if verbose:
@@ -91,7 +139,7 @@ def getLabelFromFBID(id_list, verbose = False):
         vprint = lambda *a: None
     vprint('Querying the id_list to api...')
     # Transform the list of FB ids into a string formatted
-    # For an SQL VALUE query, we get all labels with a single query
+    # For an SQL VALUE query
     freebase_ids = "\"" + "\" \"".join(id_list) + "\""
     query = '''
     PREFIX wd: <http://www.wikidata.org/entity/>
@@ -115,11 +163,8 @@ def getLabelFromFBID(id_list, verbose = False):
     results_df = pd.json_normalize(results['results']['bindings'])
     return results_df[["freebaseID.value","sLabel.value"]]
 
-# Ref : https://stackoverflow.com/questions/5980042/how-to-implement-the-verbose-or-v-option-into-a-script
-def _verboseprint(*args):
-    '''Prints all elements given as argument.'''
-    for arg in args:
-        print(arg)
+def task(chunk):
+    return getInfoFromTMDB(movie_list = chunk, verbose=False)
 
 if __name__ == '__main__':
     pass
